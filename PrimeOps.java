933e040d65a1c8b13c6464646d8d4db5d2fab36a
ramhofi <ram.hofi@post.runi.ac.il>
2024-12-02 00:23:50 +0200

    please2

--- PrimeOps.java
+++ PrimeOps.java
@@ -8,50 +8,24 @@ public class PrimeOps {
 
         // Parse the range from the first command-line argument
         int range = Integer.parseInt(args[0]);
-
-        // Handle invalid range
-        if (range < 2) {
-            System.out.println("Please enter a number greater than or equal to 2.");
-            return;
+        double counter =0;
+        System.out.println("Prime nembers up to " + range + ":");
+        for(int i = 2; i <= range; i++) {
+            if (isPrime(i)) {
+                System.out.println(i);
+                counter++;
+            }
         }
 
-        // Create an array to mark prime numbers
-        boolean[] isPrime = new boolean[range + 1];
-        for (int i = 2; i <= range; i++) {
-            isPrime[i] = true; // Assume all numbers are prime initially
-        }
-
-        // Implement the sieve logic manually
-        for (int p = 2; p * p <= range; p++) {
-            if (isPrime[p]) { // If p is still marked as prime
-                for (int i = p * p; i <= range; i += p) {
-                    isPrime[i] = false; // Mark multiples of p as not prime
+        public static boolean isPrime(int check) {
+            boolean result = true;
+            int counter = 2;
+            while (counter < check) {
+                if (check % counter == 0) {
+                    result = false;
                 }
+                counter++;
             }
+            return result;
         }
-
-        // Print the primes and count them
-        System.out.println("Prime numbers up to " + range + ":");
-        int primeCount = 0;
-        for (int i = 2; i <= range; i++) {
-            if (isPrime[i]) {
-                System.out.println(i); // Print each prime number on a new line
-                primeCount++; // Increment the count
-            }
-        }
-
-        // Total numbers between 2 and the range
-        double totalNumbers = range; // Total numbers in the range [2, n]
-    
-
-        // Calculate the percentage manually and take the floor
-        double rawPercentage = (primeCount * 100.0) / totalNumbers;
-     
-        int flooredPercentage = (int) rawPercentage; // Take the floor of the raw percentage
-
-        // Print the results in the expected format
-        System.out.println("There are " + primeCount + " primes between 2 and " + range +
-                " (" + flooredPercentage + "% are primes)");
-       
     }
-}
